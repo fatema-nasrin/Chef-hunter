be1c34fe4935e85c051c91bab55d09ee314f9238
fatema-nasrin <nasrinfatema35@gmail.com>
2023-05-04 21:34:09 +0600

    order section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/Banner/Banner';
 import Header from '../components/Header/Header';
 import Cards from '../components/Cards/Cards';
 import Footer from '../components/Footer/Footer';
+import Order from '../components/Order/Order';
 
 const Main = () => {
     return (
@@ -10,6 +11,7 @@ const Main = () => {
         <Header/>
         <Banner/>
         <Cards/>
+        <Order/>
         <Footer/>
         
         </div>
